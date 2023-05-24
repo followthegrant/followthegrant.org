4c8ae7d190f3e9ae62b22d7fcb3bf2337849ff1d
Simon Wörpel <simon.woerpel@pm.me>
2023-05-24 20:16:36 +0200

    📝 (pages) add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from "next";
 import Page from "~/components/Page";
 import IndexScreen from "~/screens/IndexScreen";
+import { TITLE, DESCRIPTION } from "~/config";
+
+export const metadata: Metadata = {
+  title: TITLE,
+  description: DESCRIPTION,
+};
 
 export default function Home() {
   return (
